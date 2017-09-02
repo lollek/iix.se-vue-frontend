c0743d126bb0906e52d96559ff68664db0f6b53e
Olle Kvarnström <olle+git@iix.se>
2017-09-02 18:40:40 +0200

    Add vue-resource

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Resource from 'vue-resource'
 import Main from '@/components/Main'
 import NotFound from '@/components/NotFound'
 import Notes from '@/components/Notes'
 
 Vue.use(Router)
+Vue.use(Resource)
 
 export default new Router({
     routes: [
