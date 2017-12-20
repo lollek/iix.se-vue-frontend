1ff878da72c7fb862a0b63114856533a7b4e5e9d
Olle Kvarnström <olle+git@iix.se>
2017-12-20 20:24:19 +0100

    Improve spinner look

--- src/components/Spinner.vue
+++ src/components/Spinner.vue
@@ -1,7 +1,10 @@
 <template>
-    <h1 class="title">Loading..
-        <i class="fa fa-spinner loader"></i>
-    </h1>
+    <div>
+        <h1 class="title has-text-centered">
+            <span>Loading..</span>
+        </h1>
+        <i class="fa fa-3x fa-spin fa-spinner"></i>
+    </div>
 </template>
 
 <script>
