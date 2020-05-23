9f5e95bab9a18087bf8a4ce4bd30e669f3a2ac8e
Olle Aid Kvarnström <olle+git@iix.se>
2020-05-23 14:30:38 +0200

    Remove games wishlist

--- src/components/wishlist/Wishlist.vue
+++ src/components/wishlist/Wishlist.vue
@@ -1,20 +1,17 @@
 <template>
     <div>
         <h1 class="title">Wishlist</h1>
-        <games-wishlist></games-wishlist>
         <books-wishlist></books-wishlist>
     </div>
 </template>
 
 <script>
-    import GamesWishlist from './GamesWishlist.vue'
     import BooksWishlist from './BooksWishlist.vue'
 
     // noinspection JSUnusedGlobalSymbols
     export default {
         components: {
-            BooksWishlist,
-            GamesWishlist
+            BooksWishlist
         },
         name: 'wishlist'
     }
